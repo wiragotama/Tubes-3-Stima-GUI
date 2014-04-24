fcff24082ad5fc971a33e60faf1b49f9b32c1fda
wiragotama <wiragotama@gmail.com>
2014-04-24 20:35:13 +0700

    firstversion

---
+++ Programs/test.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class test {
+	public static void main(String args[]) {
+		String text = String.valueOf(args[0]);
+		System.out.println(text);
+	}
+}
---
+++ submitConfiguration.php
@@ -0,0 +1,26 @@
+<!-- Author : Jan Wira Gotama Putra (13512015), Willy (13512065), Willy (13512070) -->
+<!DOCTYPE HTML>
+<html>	
+	<body> 
+		<?php 
+			/* Save to external file */
+			$curdir = getcwd(); 
+			$file = fopen("configuration.txt","w");
+			$data = $_GET["harstag"]. PHP_EOL . $_GET["lalulintas"]. PHP_EOL . $_GET["keluhan"]. PHP_EOL . $_GET["batas"] . PHP_EOL . $_GET["algo"];
+			fwrite($file,$data);
+			fclose($file);
+			
+			$curdir = getcwd(); 
+			chdir('./Programs');
+			//can only run .class file
+			$arg = $_GET["harstag"];
+			exec("java.exe test $arg > output.txt", $output, $return_var);
+			echo $return_var;
+			
+			chdir($curdir);
+			//show result
+			$url = 'home.php';
+			header( "Location: $url" );
+		?>
+	</body>
+</html>
